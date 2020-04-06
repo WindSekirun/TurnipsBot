436c69b86003955c2532e4e443f97e19cfc0a0d6
Pyxis <pyxis@uzuki.live>
2020-04-06 23:11:14 +0900

    add notify command

--- src/command/notify.ts
+++ src/command/notify.ts
@@ -2,12 +2,31 @@ import TelegramBot = require('node-telegram-bot-api');
 import { BotCommand } from '../core/BotCommand';
 import { bot } from '../core/bot';
 import * as messages from '../json/message.json';
+import { TurnipsDb } from './../core/TurnipsDb';
 import '../core/ext/String';
 
 export class Notify extends BotCommand {
   matchRegex: RegExp = /\/notify/;
 
   async onMatch(message: TelegramBot.Message, match: RegExpMatchArray): Promise<void> {
-    console.log(message);
+    let db = await TurnipsDb.getInstance();
+    let userName = (message?.from?.username || message?.from?.first_name) ?? '';
+    let userId = message?.from?.id + '';
+    let chatId = message.chat.id;
+
+    // 유저가 존재하는지 체크
+    let exists = await db.existsUser(userId);
+    if (!exists) {
+      let returnMessage = messages.fail_non_exists.format(userName);
+      bot.sendMessage(chatId, returnMessage);
+      return;
+    }
+
+    let nowNotify = await db.isNotifyUser(userId);
+    let newNotify = !nowNotify
+    await db.setNotifyUser(userId, newNotify);
+
+    let returnMessage = messages.notify_result.format(newNotify ? '사용함' : '사용하지 않음');
+    bot.sendMessage(chatId, returnMessage)
   }
 }
