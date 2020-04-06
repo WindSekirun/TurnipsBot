750a016bebb5e530bb680f13591e56300abe3d98
Pyxis <pyxis@uzuki.live>
2020-04-06 23:14:25 +0900

    add result, start command

--- src/command/result.ts
+++ src/command/result.ts
@@ -2,12 +2,44 @@ import TelegramBot = require('node-telegram-bot-api');
 import { BotCommand } from '../core/BotCommand';
 import { bot } from '../core/bot';
 import * as messages from '../json/message.json';
+import { TurnipsDb } from './../core/TurnipsDb';
+import { ControlResult } from './../core/ControlResult';
 import '../core/ext/String';
+import * as Keyboard from '../core/Keyboard';
 
 export class Result extends BotCommand {
   matchRegex: RegExp = /\/result/;
 
   async onMatch(message: TelegramBot.Message, match: RegExpMatchArray): Promise<void> {
-    console.log(message);
+    let db = await TurnipsDb.getInstance();
+    let userName = (message?.from?.username || message?.from?.first_name) ?? '';
+    let userId = message?.from?.id + '';
+    let chatId = message.chat.id;
+
+    // 유저가 존재하는지 체크
+    let exists = await db.existsUser(userId);
+    if (!exists) {
+      let returnMessage = messages.fail_non_exists.format(userName);
+      bot.sendMessage(chatId, returnMessage);
+      return;
+    }
+
+    let controlResult = new ControlResult();
+    let fileUrl = controlResult.getUrl(userId);
+
+    // Keyboard object 구성
+    let options = new Keyboard.SendMessageOptions();
+    let button = new Keyboard.InlineKeyboardButton();
+    button.text = messages.result_btn;
+    button.url = fileUrl;
+    let buttonArray: Keyboard.InlineKeyboardButton[] = [button];
+    let markup = new Keyboard.InlineKeyboardMarkup();
+    markup.inline_keyboard = [];
+    markup.inline_keyboard.push(buttonArray);
+    options.reply_markup = markup;
+
+    // 사용자 반환
+    let returnMessage = messages.result_result.format(userName);
+    bot.sendMessage(chatId, returnMessage, options);
   }
 }
--- src/command/start.ts
+++ src/command/start.ts
@@ -1,12 +1,14 @@
 import TelegramBot = require('node-telegram-bot-api');
 import { BotCommand } from '../core/BotCommand';
 import { bot } from '../core/bot';
+import * as messages from '../json/message.json';
 import '../core/ext/String';
 
 export class Start extends BotCommand {
   matchRegex: RegExp = /\/start/;
 
   async onMatch(message: TelegramBot.Message, match: RegExpMatchArray): Promise<void> {
-    console.log(message);
+    let returnMessage = messages.start_result
+    bot.sendMessage(message.chat.id, returnMessage)
   }
 }
