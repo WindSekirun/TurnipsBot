7692f9bc45488596fb9dd3f3d97f79dfecf4314d
Pyxis <Pyxis@uzuki.live>
2020-04-12 20:50:52 +0900

    remove jest-extended, change message in release

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   runner: 'jest-serial-runner',
-  setupFilesAfterEnv: ['./jest.setup.js', 'jest-extended'],
+  setupFilesAfterEnv: ['./jest.setup.js'],
 };
